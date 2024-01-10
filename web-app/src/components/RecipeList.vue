cd13f670462194cec0eecdf7dd2157816524c44a
budaala <budaala4@gmail.com>
2024-01-10 14:18:08 +0100

    navbar styling

---
+++ web-app/src/components/RecipeList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container-md">
+        <h1>Witaj w Zesty!</h1>
+        <h2>How you doin'?</h2>
+        <p>tu się pojawi main content</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'RecipeList',
+        components: {}
+    }
+
+</script>
+
+<style scoped>
+</style>
