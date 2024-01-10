52c42734d4ba597a515c80cae730a35146c5a2aa
budaala <budaala4@gmail.com>
2024-01-10 12:29:16 +0100

    transfered to web app folder

---
+++ web-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <navbar>
+
+  </navbar>
+  <h1>Hello</h1>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+</style>
